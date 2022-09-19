77e1910a096ca66a6b7058af1d0ae413de97ae5a
ntthuc <ntthuc@tma.com.vn>
2022-09-19 16:49:27 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import MenuList from './components/MenuList';
 
 function App() {
+  const resource = [
+    {
+      itemName: "Caesar's Salad",
+      description: "The original Caesar's Salad recipe",
+      foodImage: 'https://bit.ly/2GJFu5W',
+      price: 12,
+      isFavorite: false,
+    },
+    {
+      itemName: 'Spaghetti Carbonara',
+      description: "Better than your nonna's! All local and fresh ingredients",
+      foodImage: 'https://bit.ly/2GBnz1n',
+      price: 15,
+      isFavorite: false,
+    },
+    {
+      itemName: 'Grilled Whole Fish',
+      description: 'Fish of the day, grilled whole with a side of vegetables',
+      foodImage: 'https://bit.ly/3d6iKsJ',
+      price: 20,
+      isFavorite: false,
+    },
+    {
+      itemName: 'Steak Florentine',
+      description: 'Meat!',
+      foodImage: 'https://bit.ly/3ddiSH1',
+      price: 30,
+      isFavorite: false,
+    }]
+
+    const [foodItems, setFootItems] = useState(resource)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ConBo Restaurant's Menu</h1>
+      <MenuList foodItems={foodItems} setFootItems={setFootItems}/>
     </div>
   );
 }
