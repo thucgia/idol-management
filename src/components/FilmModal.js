d41804b8f60d999b67fba5c7b4fffaec872875fa
ntthuc <ntthuc@tma.com.vn>
2022-09-21 15:47:40 +0700

    update: upload image to freeimage.host api

--- src/components/FilmModal.js
+++ src/components/FilmModal.js
@@ -1,44 +1,74 @@
+import axios from 'axios';
 import React, { useState } from 'react';
-import './FilmEdit.css'
+import './FilmModal.css'
 
 function FilmModal(props) {
     const [film, setFilm] = useState(props.film ? 
                             {id: props.film.id, fname: props.film.fname, release_year: props.film.release_year, actors: props.film.actors, image: props.film.image, description: props.film.description, isFavorite : props.film.isFavorite} : 
                             {fname: "", release_year: "", actors: "", image: "", description: "", isFavorite : ""})
+
+    const [image, setImage] = useState(props.film ? film.image : null)
+
     const validateSelect = () => {
         return film.actors.length <= 2;
     }
-    const save = (event) => {
-        event.preventDefault()
-        if (!validateSelect()) {
-            alert("Only 2 Actor!")
-            return
-        }
+
+    const createFilm = (image_url = false) => {
         props.setFilmItems(prevState => {
             // update
             if (film.id) {
                 let index = prevState.findIndex(item => item.id === props.film.id)
                 let items = [...prevState]
                 let actors = Array.from(film.actors, actor => parseInt(actor))
-                items[index] = {...film, "actors" : actors}
+                
+                if (image_url) items[index] = {...film, "actors" : actors, "image" : image_url}
+                else items[index] = {...film, "actors" : actors}
                 return items
             } else {
-                // add
+                // create
                 let index = prevState.reduce((prev, current) => { return prev.id > current.id ? prev : current }).id
                 let items = [...prevState]
                 let actors = Array.from(film.actors, actor => parseInt(actor))
-                let item = {...film, "id" : index + 1, "actors" : actors}
+                let item = {}
+                if (image_url) item = {...film, "id" : index + 1, "actors" : actors, "image" : image_url}
+                else item = { ...film, "id" : index + 1, "actors" : actors }
                 items.push(item)
+                console.log(items)
                 return items
             }
         })
+    }
 
-        props.setShowing(() => !props.isShowing)
+    const save = (event) => {
+        event.preventDefault()
+        if (!validateSelect()) {
+            alert("Only 2 Actor!")
+            return
+        }
+
+        if ((props.film && (props.film.image !== image)) || (!props.film && image)) {
+            let url = "https://freeimage.host/api/1/upload?key=6d207e02198a847aa98d0a2a901485a5"
+            const data = new FormData()
+            data.append("source", film.image)
+            data.append("format", "json")
+            axios.post(url, data)
+            .then(res => {
+                createFilm(res.data.image.display_url)
+                props.setShowing(() => !props.isShowing)
+            })
+            .catch(err => { throw err })
+        } else {
+            createFilm()
+            props.setShowing(() => !props.isShowing)
+        }
     }
     const collectData = (event) => {
         if (event.target.name === "actors") {
             let value = Array.from(event.target.selectedOptions, option => option.value)
             setFilm({...film, [event.target.name] : value})
+        } else if (event.target.name === "image" && event.target.files && event.target.files[0]){
+            setImage(URL.createObjectURL(event.target.files[0]))
+            setFilm({...film, "image" : event.target.files[0]})
         } else {
             setFilm({...film, [event.target.name] : event.target.value})
         }
@@ -69,7 +99,8 @@ function FilmModal(props) {
                                     <label htmlFor="image">Image</label>
                                 </div>
                                 <div className="col-75">
-                                    <input type="text" id="image" name="image" defaultValue={film.image ? film.image : ""} placeholder="Type here.." onChange={collectData}/>
+                                    <input type="file" id="image" name="image" onChange={collectData}/>
+                                    <img id="previewImg" alt='Preview' src={image}/>
                                 </div>
                             </div>
                             <div className="row">
